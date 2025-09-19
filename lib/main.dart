e43aa10e7824bcc9b121428a4d6f2861200c29f9
quyenbu123 <caodangquyen.pt05@gmail.com>
2025-09-19 10:56:33 +0700

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import '../view/on_boarding/welcome_screen.dart';
-import 'firebase_options.dart';
+import 'database/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
