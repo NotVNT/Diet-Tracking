71f97e74347ded2bc7b77ae48029a7a04f4a9b30
Vinh Ngoc Tran <play13072004@gmail.com>
2026-01-04 20:37:11 +0700

    remove unused import

--- test/features/upload_video/presentation/widgets/video_preview_widget_test.dart
+++ test/features/upload_video/presentation/widgets/video_preview_widget_test.dart
@@ -1,10 +1,5 @@
-import 'package:diet_tracking_project/features/upload_video/presentation/widgets/video_preview_widget.dart';
-import 'dart:io';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:video_player_platform_interface/video_player_platform_interface.dart';
-
 import '../../helpers/fake_video_player_platform.dart';
 
 void main() {
